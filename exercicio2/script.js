0b834bcc6c9b4e3a0d7ac3c0aa07e98ed9bc6d97
Gustavo <gustavosoares2790@gmail.com>
2023-09-12 21:10:33 -0300

    exercícios de fixação condicionais II

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,41 @@
-let nacionalidade = prompt("Digite aqui a nacionalidade").toLowerCase()
+/*
+let nacionalidade = prompt("Digite aqui sua nacionalidade:").toLowerCase()
 
 if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!")
+    console.log("A pessoa é do Brasil!")
 } else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!")
+    console.log("A pessoa é da Argentina!")
 } else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!")
+    console.log("A pessoa é do Uruguai!")
 } else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!")
+    console.log("A pessoa é do Chile!")
 } else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!")
+    console.log("A pessoa é da Colômbia!")
 } else{
-    console.log("nacionalidade não encontrada")
+    console.log("Nacionalidade não encontrada!")
 }
+*/
+
+let nacionalidade = prompt("Digite aqui sua nacionalidade:").toLowerCase()
+
+switch (nacionalidade){
+    case "brasileira":
+        console.log(`A pessoa é ${nacionalidade}`)
+        break
+    case "argentina":
+        console.log(`A pessoa é ${nacionalidade}`)
+        break
+    case "uruguaia":
+        console.log(`A pessoa é ${nacionalidade}`)
+        break
+    case "chilena":
+        console.log(`A pessoa é ${nacionalidade}`)
+        break
+    case "colombiana":
+        console.log(`A pessoa é ${nacionalidade}`)
+        break
+    default:
+        console.log('Nacionalidade não encotrada')
+
+}
+
